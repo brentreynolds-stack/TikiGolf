7d77c4cf8bbddb3b8cf977b9c2f7791c9e545ca2
Brent Reynolds <brentreynoldsair@Brents-MacBook-Air.local>
2026-09-14 20:57:04 -0500

    Tiki Golf 2026

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// NEVER commit this file to GitHub or share it publicly
+const TIKI_CONFIG = {
+  SHEETS_API_KEY:   'AIzaSyDLB0aoT45FjlvnXWr05DfSntK2qo_fKcM',
+  SPREADSHEET_ID:   '1tfXfmLIuTzkm9uMlZiqsQpIR0cItbvxJdmhZdh8B0hM',
+  GOOGLE_CLIENT_ID: '155719842078-9mfokrn17j38d48truabftchnjhotmib.apps.googleusercontent.com',
+  ADMIN_PIN:        '6007',
+}
